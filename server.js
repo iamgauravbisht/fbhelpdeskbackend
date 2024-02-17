8e52635bbac63326cb8bb67f2f853ba618d581b4
Gaurav <gauravbisht154@gmail.com>
2024-02-17 17:00:43 +0530

    env updated

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get("/webhook", (req, res) => {
   // Check if a token and mode is in the query string of the request
   if (mode && token) {
     // Check the mode and token sent is correct
-    if (mode === "subscribe" && token == verifyToken) {
+    if (mode == "subscribe" && token == verifyToken) {
       // Respond with the challenge token from the request
       console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
@@ -176,7 +176,7 @@ app.get("/messaging-webhook", (req, res) => {
   // Check if a token and mode is in the query string of the request
   if (mode && token) {
     // Check the mode and token sent is correct
-    if (mode === "subscribe" && token == verifyToken) {
+    if (mode == "subscribe" && token == verifyToken) {
       // Respond with the challenge token from the request
       console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
