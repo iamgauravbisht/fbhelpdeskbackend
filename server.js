8320f409593a7e25850fbd79c218d9fac7137025
Gaurav <gauravbisht154@gmail.com>
2024-02-17 19:00:57 +0530

    cors updated

--- server.js
+++ server.js
@@ -28,7 +28,13 @@ const port = process.env.PORT || 5000;
 //   allowedHeaders: "Content-Type, Authorization",
 // }; corsOptions
 
-app.use(cors());
+// app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(express.json());
 app.use("/auth", authRouter);
